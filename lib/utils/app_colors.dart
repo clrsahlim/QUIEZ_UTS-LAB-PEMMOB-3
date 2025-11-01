e52638b911c6f8015c151d50900e228f86cd0dff
clrsahlim <clarissaliem17.2@gmail.com>
2025-11-01 22:10:12 +0700

    splash screen, setup font dan logo apk

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color beige = Color(0xFFF6EFDC);
+  static const Color yellow = Color(0xFFFDD87D);
+  static const Color red = Color(0xFFCE4636);
+  static const Color blue = Color(0xFFADCAD8);
+  static const Color green = Color(0xFF98BFA6);
+  static const Color pink = Color(0xFFFAB6BD);
+  static const Color black = Color(0xFF000000);
+  static const Color white = Color(0xFFFFFFFF);
+}
